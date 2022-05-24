4bde42c52953dd584797480f0f6b923aca5640fd
jpbullalayao <jpbullalayao@gmail.com>
2022-05-24 16:23:10 -0700

    Add table package

---
+++ packages/core-components/Table/src/Table/Table.stories.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import {
+  ComponentMeta,
+  ComponentStory,
+} from '@storybook/react';
+
+import {
+  Table,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from '..';
+
+export default {
+  title: 'Table',
+  component: Table,
+} as ComponentMeta<typeof Table>;
+
+export const Primary: ComponentStory<typeof Table> = () => (
+  <Table>
+    <Thead>
+      <Tr>
+        <Th>Table Header 1</Th>
+        <Th>Table Header 2</Th>
+        <Th>Table Header 3</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>Table Cell 1</Td>
+        <Td>Table Cell 2</Td>
+        <Td>Table Cell 3</Td>
+      </Tr>
+      <Tr>
+        <Td>Table Cell 4</Td>
+        <Td>Table Cell 5</Td>
+        <Td>Table Cell 6</Td>
+      </Tr>
+      <Tr>
+        <Td>Table Cell 7</Td>
+        <Td>Table Cell 8</Td>
+        <Td>Table Cell 9</Td>
+      </Tr>
+    </Tbody>
+    <Tfoot>
+      Table footer
+    </Tfoot>
+  </Table>
+);
